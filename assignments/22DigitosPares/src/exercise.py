4ffca93396a823d690e934d707ebb2ca88e34198
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-30 20:23:01 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
+    numeros=input('Inserte un número:')
+a=0
+if int(numeros[0])%2 ==0:
+    a=a+1
+else: a=a+0
+if int(numeros[1])%2 ==0:
+    a=a+1
+else: a=a+0
+if int(numeros[2])%2 ==0:
+    a=a+1
+else: a=a+0
+if int(numeros[3])%2 ==0:
+    a=a+1
+else: a=a+0
+
+print('El número de dígitos pares son:',a)
     pass
 
 
