e12595bc12479ce14337398b92a7f8c870d97eca
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-30 20:09:10 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+palabras=int(input('Inserta el número de palabras:'))
+
+if palabras >0:
+    a=palabras/475
+    b=math.ceil(a)
+    print(b)
+    cobroed=b*60
+    descuento=cobroed*0.1
+    costototal=cobroed-descuento
+    print ('El costo de publicación es:',costototal)
+
+else: print('Número no válido')
     pass
 
 
